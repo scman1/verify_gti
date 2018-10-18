e91135df21c0a56bf317503ffc922e7d13789726
Abraham Nieva <a_nieva@hotmail.com>
2018-10-18 14:53:22 +0100

    minimise added borders

--- herbarium_gtiv.py
+++ herbarium_gtiv.py
@@ -118,7 +118,7 @@ def pixels_from_stds(img_height, img_width):
     #print(img_width_inch, img_height_inch)
     ppi_x = round(max_width * dpi_predicted)
     ppi_y = round(max_height * dpi_predicted)
-        
+      
     pix_add_x = int(round((max_width - img_width_inch) * dpi_predicted))
     pix_add_y = int(round((max_height - img_height_inch) * dpi_predicted))
 
@@ -153,6 +153,23 @@ def pixels_calculated(img_height, img_width):
     pix_add_x = int(round((max_width - img_width_inch) * dpi_predicted))
     pix_add_y = int(round((max_height - img_height_inch) * dpi_predicted))
     return dpi_predicted,pix_add_x, pix_add_y
+
+def pixels_for_ratio(img_height, img_width):
+    dpi_predicted=standard_based_dpi(img_height, img_width)
+
+    ppi_x = int(round(max_width * dpi_predicted))
+    ppi_y = int(round(max_height * dpi_predicted))
+
+    # calculate pixels to add to maintain aspect ratio for x and y
+    pix_add_x  = int(round(ppi_x * img_height/ppi_y) - img_width)
+    pix_add_y = int(round(img_width * ppi_y / ppi_x) - img_height)
+
+    if pix_add_x > 0:
+      pix_add_y = 0
+    elif pix_add_y > 0:
+        pix_add_x = 0
+        
+    return dpi_predicted,pix_add_x, pix_add_y
     
 # add borers to all images in source directory and save them to dest directory  
 def add_borders(source_dir, dest_dir):
@@ -164,7 +181,7 @@ def add_borders(source_dir, dest_dir):
         img = Image.open(s_image)
         img_width, img_height = img.size
         #try to aproximate from standard sizes 
-        dpi_predicted,  pix_add_x, pix_add_y = pixels_from_stds(img_height, img_width)
+        dpi_predicted, pix_add_x, pix_add_y = pixels_for_ratio(img_height, img_width)
         
         print (i, source_filename.name,img_width, img_height, dpi_predicted, pix_add_x, pix_add_y)
         img_dpis[s_image] = (i, source_filename.name,img_width, img_height, dpi_predicted, pix_add_x, pix_add_y)
@@ -189,8 +206,9 @@ def add_borders(source_dir, dest_dir):
     print(img_dpis)
 
 # shrink images to the specified dpi width and height
-def shrink_images(working_dir, width_to, height_to):
-    for filepath in sorted(working_dir.glob('*')):
+def shrink_images(source_dir, dest_dir, width_to, height_to):
+    dest_dir.mkdir(parents=True, exist_ok=True)
+    for filepath in sorted(source_dir.glob('*')):
         if "JPG" in filepath.name or "png" in filepath.name:
             #read the image
             img = cv2.imread(str(filepath), cv2.IMREAD_COLOR)
@@ -205,9 +223,9 @@ def shrink_images(working_dir, width_to, height_to):
             res = cv2.resize(img,None,fx=shrk_factor, fy=shrk_factor, interpolation = cv2.INTER_NEAREST)
             # crop image
             if "JPG" in filepath.name:
-                cv2.imwrite(str(Path(working_dir,filepath.name)),res,params_jpg)
+                cv2.imwrite(str(Path(dest_dir,filepath.name)),res,params_jpg)
             elif "png" in filepath.name:
-                cv2.imwrite(str(Path(working_dir,filepath.name)),res,params_png)
+                cv2.imwrite(str(Path(dest_dir,filepath.name)),res,params_png)
 
 def pixel_sizes(directory, width_to, height_to ):
     # verify pixel dimensions
@@ -229,21 +247,21 @@ def get_unique_colours_2(img):
 
 def get_colour_count(img):
     aimg= numpy.asarray(img)
-    return Counter([tuple(colors) for i in aimg for colors in i])
+    return Counter([tuple(colours) for i in aimg for colours in i])
 
 # PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
-def getcolorlist(img):
+def getcolourlist(img):
     rgb_img = img.convert('RGB') # create the pixel map
-    colorlist={}
+    colourlist={}
     for i in range(img.size[0]):    # for every col:
         for j in range(img.size[1]):    # For every row
             r, g, b = rgb_img.getpixel((i,j)) # set the colour accordingly
-            color=str(r)+","+str(g)+","+str(b)
-            if color in colorlist:
-                colorlist[color]+=1
+            colour=str(r)+","+str(g)+","+str(b)
+            if colour in colourlist:
+                colourlist[colour]+=1
             else:
-                colorlist[color]=1
-    return colorlist
+                colourlist[colour]=1
+    return colourlist
 
 
 def exclude_used(dest_dir, used_dir):
@@ -275,33 +293,37 @@ def correct_label_colours(filename_labels):
     img2 = img_labels
     for i in range(height):
         for j in range(width):
-            color = img2[i,j]
-            if color[0] < 100:
-                color[0] = 0
+            colour = img2[i,j]
+            if colour[0] < 100:
+                colour[0] = 0
             else:
-                color[0] = 255
-            if color[1] < 100:
-                color[1] = 0
+                colour[0] = 255
+            if colour[1] < 100:
+                colour[1] = 0
             else:
-                color[1] = 255
-            if color[2] < 100:
-                color[2] = 0
+                colour[1] = 255
+            if colour[2] < 100:
+                colour[2] = 0
             else:
-                color[2] = 255
-            img2[i,j] = color
+                colour[2] = 255
+            img2[i,j] = colour
     cv2.imwrite(str(filename_labels),img2)
 
-def verify_label_colors(dest_dir):
+def verify_label_colours(source_dir, dest_dir):
+    dest_dir.mkdir(parents=True, exist_ok=True)
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
-    #counter = 0
-    for dest_filename in sorted(dest_dir.glob('*labels.png')):
+    #copy all files to dest_dir
+    for filepath in sorted(source_dir.glob('*')):
+         shutil.copy(str(filepath), Path(dest_dir, filepath.name))
+                                
+    for dest_filename in sorted(source_dir.glob('*labels.png')):
         s_filename = str(dest_filename)
         img = Image.open(str(s_filename))
         colours = get_colour_count(img)        
         colourlist=[*colours]
         incorrect_label_colour = 0
-        for color in colourlist:
-            if color not in [(255, 255, 0), (0, 0, 0), (255, 255, 255), (255, 0, 0)]:
+        for colour in colourlist:
+            if colour not in [(255, 255, 0), (0, 0, 0), (255, 255, 255), (255, 0, 0)]:
                 incorrect_label_colour = 1
                 break
         if len(colours)>4 or incorrect_label_colour == 1:
@@ -312,26 +334,26 @@ def verify_label_colors(dest_dir):
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
 
 # change colours with counts smaller than 500 to the background colour
-def correct_instance_colors(filename_labels):
+def correct_instance_colours(filename_labels):
     img_instances = cv2.imread(str(filename_labels), cv2.IMREAD_COLOR)
     height,width,channels = img_instances.shape
 
-    colorlist=get_colour_count(img_instances)
-    #print("initial colour count", len(colorlist))
-    #print(colorlist)
-    bigcolors ={}
+    colourlist=get_colour_count(img_instances)
+    #print("initial colour count", len(colourlist))
+    #print(colourlist)
+    bigcolours ={}
     countuniques = 0
-    for color in colorlist:
-        if colorlist[color] < 500:
+    for colour in colourlist:
+        if colourlist[colour] < 500:
             countuniques += 1
         else:
-            bigcolors[color]=colorlist[color]
+            bigcolours[colour]=colourlist[colour]
     img2 = img_instances
     background = (0, 0, 0)
     for i in range(height):
         for j in range (width):
-            color = img2[i,j]
-            if not (tuple(color) in bigcolors):
+            colour = img2[i,j]
+            if not (tuple(colour) in bigcolours):
                 img2[i,j] = background
     cv2.imwrite(str(filename_labels),img2)
 
@@ -340,23 +362,23 @@ def correct_instance_backgrounds(filename_labels):
     img_instances = cv2.imread(str(filename_labels), cv2.IMREAD_COLOR)
     height,width,channels = img_instances.shape
 
-    colorlist=get_unique_colours(img_instances)
+    colourlist=get_unique_colours(img_instances)
     corrections = False
     corrections_list = []
     #first check if there are colours close to black in the image
-    for color in colorlist:
-        if color[0] < 50 and color[1] < 50 and color[2] < 50 and \
-           color != (0,0,0):
-            corrections_list.append(list(color))
+    for colour in colourlist:
+        if coluor[0] < 50 and colour[1] < 50 and colour[2] < 50 and \
+           colour != (0,0,0):
+            corrections_list.append(list(colour))
             corrections = True
     # if there are colours close to black, get a list of corresponding pixels
     # and correct them (make them black)
     if corrections:
         background = (0, 0, 0)
-        for color_match in corrections_list:
+        for colour_match in corrections_list:
             img2 = img_instances
             # numpy where and all help in selecting only matching colour pixels
-            indices = numpy.where(numpy.all(img2 == color_match, axis=-1))
+            indices = numpy.where(numpy.all(img2 == colour_match, axis=-1))
             coordinates = list(zip(indices[0], indices[1]))
             for i, j in coordinates:
                img2[i,j] = background 
@@ -373,21 +395,21 @@ def compare_instances_to_labels(filename_labels):
     height,width,channels = img_instances.shape
 
     colorlist=get_colour_count(img_instances)
-    #print("initial colour count", len(colorlist))
-    #print(colorlist)
-    bigcolors ={}
+    #print("initial colour count", len(colourlist))
+    #print(colourlist)
+    bigcolours ={}
     countuniques = 0
-    for color in colorlist:
-        if colorlist[color] < 500:
+    for colour in colourlist:
+        if colourlist[colour] < 500:
             countuniques += 1
         else:
-            bigcolors[color]=colorlist[color]
+            bigcolours[coluor]=colourlist[colour]
     img2 = img_instances
     background = (0, 0, 0)
     for i in range(height):
         for j in range (width):
-            color = img2[i,j]
-            if not (tuple(color) in bigcolors):
+            colour = img2[i,j]
+            if not (tuple(colour) in bigcolours):
                 img2[i,j] = background
     cv2.imwrite(str(filename_labels),img2)
     
@@ -402,7 +424,7 @@ def verify_instance_borders(dest_dir):
         if len(colours)>20:
             #print(dest_filename.name, colours, len(colours), "needs correcting colours")
             print(dest_filename.name, len(colours), "needs correcting colours")
-            correct_instance_colors(dest_filename)
+            correct_instance_colours(dest_filename)
         else:
             print(dest_filename.name, len(colours), "ok")
     #    if counter > 90:
@@ -422,8 +444,8 @@ def verify_instance_backgrounds(dest_dir):
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
 
 #get all pixel coordinates for a given colour
-def getobject(img,color):
-    indices = numpy.where(numpy.all(img == color, axis=-1))
+def getobject(img,colour):
+    indices = numpy.where(numpy.all(img == colour, axis=-1))
     pixels = list(zip(indices[0], indices[1]))
     return pixels
 
@@ -450,32 +472,32 @@ def rename_files(source_dir, dest_dir):
             shutil.copy(str(filepath), Path(dest_dir, workfile))
 
 # Directory containing the new set of segmented images
-source_dir = Path(Path().absolute().parent, "herbariumsheets","sample04")
+source_dir = Path(Path().absolute().parent, "herbariumsheets","sample04s")
 # Directory for processing and verifying the new set of segmented images
-work_dir = Path(Path().absolute().parent, "herbariumsheets","sample04", "to_process")
-# Directory containing the already used set of segmented images (renamed and formated)
-used_dir = Path(Path().absolute().parent, "herbariumsheets", "TrainingHerbariumSheets0296dpi")
+work_dir = Path(Path().absolute().parent, "herbariumsheets","sample04s", "to_process")
 
-### 1. rename all files to match the pattern used by the learning script
-##rename_files(source_dir, work_dir)
-##
+# 1. rename all files to match the pattern used by the learning script
+rename_files(source_dir, work_dir)
+
 ### 2. verify if new set contains already used used images and
 ###    if so, move them to another directory
+### Directory containing the already used set of segmented images (renamed and formated)
+##used_dir = Path(Path().absolute().parent, "herbariumsheets", "TrainingHerbariumSheets0296dpi")
 ##exclude_used(work_dir, used_dir)
-##
-### 3. add borders to all images
-##borders_dir = Path(Path().absolute().parent, "herbariumsheets","sample04", "withborders")
-##add_borders(work_dir, borders_dir)
+
+# 3. add borders to all images
+borders_dir = Path(Path().absolute().parent, "herbariumsheets","sample04s", "withborders")
+add_borders(work_dir, borders_dir)
 # 4. shrink images to standard size and resolution
 #    1169, 1764 for 96 dpi
 #     877, 1323 for 72 dpi
-##dest_dir = Path(Path().absolute().parent, "herbariumsheets","sample04", "resized")
-##shrink_images(dest_dir, max_width_96, max_height_96)
-### verify pixel sizes
-##pixel_sizes(dest_dir, max_width_96, max_height_96)
-# 5. verify and correct label colors (solid red, white, yellow and black)
-dest_dir = Path(Path().absolute().parent, "herbariumsheets","sample04", "colorcorrect")
-verify_label_colors(dest_dir)
+resize_dir = Path(Path().absolute().parent, "herbariumsheets","sample04s", "resized")
+shrink_images(borders_dir, resize_dir, max_width_96, max_height_96)
+# verify pixel sizes
+pixel_sizes(resize_dir, max_width_96, max_height_96)
+# 5. verify and correct label colours (solid red, white, yellow and black)
+colour_dir = Path(Path().absolute().parent, "herbariumsheets","sample04s", "colourcorrect")
+verify_label_colours(resize_dir, colour_dir)
 ### 6.verify and match instances to labels
 ### 6.1 correct the borders of the instances eliminating colours with small count
 ###verify_instance_borders(dest_dir)
@@ -486,7 +508,7 @@ verify_label_colors(dest_dir)
 ##
 ### 7. grow all instances (compensate border correction)
 ##
-### 8. recolor instances backgrounds (make them all light instead of black)
+### 8. recolour instances backgrounds (make them all light instead of black)
 ###  
 ### 9. use relative positions of labels and corrected instances to create new
 ###    labels that match instance sizes

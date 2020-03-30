4808f71a5b4f192d5d4ba624b045d60e6e1812bf
scman1 <a_nieva@hotmail.com>
2020-03-30 16:19:21 +0100

    compare GT to Predictions 04

--- countcolours.py
+++ countcolours.py
@@ -2,6 +2,23 @@ import numpy as np
 from skimage import io, morphology, measure
 from sklearn.cluster import KMeans
 from datetime import datetime
+import csv
+from pathlib import Path
+import sys
+
+# writes data to the given file name
+def write_csv_data(values, filename):
+    fieldnames = []
+    for item in values.keys():
+        for key in values[item].keys():
+            if not key in fieldnames:
+                fieldnames.append(key)
+    #write back to a new csv file
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for key in values.keys():
+            writer.writerow(values[key])
 
 # get list of unique colour, with a count of the pixels
 def get_colours_list(img):
@@ -212,78 +229,124 @@ def get_shapes_per_colour(filename):
             objects[this_colour] = all_paths
     return objects
 
+# retrieve test files using tr/ts proportions
+def test_files_list(source_dir,tr=8,ts=2):
+    i_counter = 0
+    files_list = []
+    for filepath in sorted(source_dir.glob('*.JPG')):
+        i_counter += 1
+        if i_counter > tr:
+            files_list.append(Path(filepath).name[:-4])
+            if i_counter == tr+ts:
+                i_counter = 0
+    return files_list
 
-print("Start",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+# ground truth dataset
+def get_gt_values(argv,label_colors=None):
+    print("Start",datetime.now().strftime("%Y/%m/%d %H:%M:%S"), 'Arguments:', argv)
+    try:
+        gt_path = argv[0]
+        pr_path = argv[1]
+        out_file = argv[2]
+        train_prop = int(argv[3])
+        test_prop = int(argv[4])
+    except:
+        print("provide five arguments:"+
+              "\n -string ground truths path\n -string predictions path"+
+              "\n -string output filename (csv)\n -integer proportion train set"+
+              "\n -integer proportion test/eval set")
+        return
+    gt_dir= Path(gt_path)
+    # predictions dataset
+    pr_dir = Path(pr_path)
+    # paths for ground truth set on semseg directory
+    images_dir = gt_dir / 'images'
+    labels_dir = gt_dir / 'labels'
+    instances_dir = gt_dir / 'instances'
 
-filename = "010668463_816414_1428267"
-filename = "010653012_816393_1428658"
-filename = "010646759_816445_1431072"
+    # get a list of the files to compare
+    file_list = test_files_list(images_dir,train_prop,test_prop)
 
-# GT labels file
-gt_lbl = 'test/data/raw/labels/{}_labels.png'.format(filename) # labels_path / gt_data[indx]['labels']
-# GT instances file
-gt_ins = 'test/data/raw/instances/{}_instances.png'.format(filename) # instances_path / gt_data[indx]['instances']
-#print(predictions_path)
-# predictions labels file
-pr_lbl = 'test/predictions/{}_labels.png'.format(filename) # predictions_path / gt_data[indx]['labels']
-# predictions instances file
-pr_ins = 'test/predictions/{}_instances.png'.format(filename) # predictions_path / gt_data[indx]['instances']
+    print("Start",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    print(file_list)
 
-#***************************************************
-# Calculate TP, TN, FP, and FN for earch prediction
-# a) open the predictions instance file
-# b) for each colour in the instance, get borders.
-pr_objects = get_shapes_per_colour(pr_ins)
+    ob_values = {}
+    indx = 1
 
-pr_lbl_img = io.imread(pr_lbl)
-# get image size and number of pixel elements
-rows, cols, bands = pr_lbl_img.shape
-# discard alpha channel
-if bands > 3:
-    pr_lbl_img = pr_lbl_img[:,:,:3]
-    bands = 3
-gt_lbl_img = io.imread(gt_lbl)
-# get image size and number of pixel elements
-rows, cols, bands = gt_lbl_img.shape
-# discard alpha channel
-if bands > 3:
-    gt_lbl_img = gt_lbl_img[:,:,:3]
-    bands = 3
+    for filename in file_list:
+        # GT labels file
+        gt_lbl = Path(labels_dir, filename+'_labels.png')
+        # GT instances file
+        gt_ins = Path(instances_dir, filename+'_instances.png')
+        # predictions labels file
+        pr_lbl = Path(pr_dir, filename+'_labels.png')
+        # predictions instances file
+        pr_ins = Path(pr_dir, filename+'_instances.png')
 
+        pr_lbl_img = io.imread(pr_lbl)
+        # get image size and number of pixel elements
+        rows, cols, bands = pr_lbl_img.shape
+        # discard alpha channel
+        if bands > 3:
+            pr_lbl_img = pr_lbl_img[:,:,:3]
+            bands = 3
+        gt_lbl_img = io.imread(gt_lbl)
+        # get image size and number of pixel elements
+        rows, cols, bands = gt_lbl_img.shape
+        # discard alpha channel
+        if bands > 3:
+            gt_lbl_img = gt_lbl_img[:,:,:3]
+            bands = 3
 
-pr_values = {}
-indx = 1
-for an_object in pr_objects:
-    print('Colour: {}  >>  Fragments: {}'.format(an_object, len(pr_objects[an_object])))
-    # c) get the types assigned to each object fragment in predictions and GT
-    # d) compare to GT labels  to get TP and FP
-    for fragment in pr_objects[an_object]:
-        f_centre = getcontourcentre(fragment)
-        assign_pr_class = assignclass(fragment, pr_lbl_img)
-        gt_class = tuple(gt_lbl_img[f_centre])
-        print(an_object, "ground truth:",gt_class, "predicted:", assign_pr_class)
-        pr_values[indx] = {"obj_colour":an_object, "ground truth":gt_class, "predicted":assign_pr_class}
-        indx +=1
+        #print(gt_ins, gt_lbl, pr_ins, pr_lbl)
+        #***************************************************
+        # Calculate TP, TN, FP, and FN for earch prediction
+        #***************************************************
+        # e) open the GT instance file
+        # f) for each colour in the instance, get borders
+        gt_objects = get_shapes_per_colour(gt_ins)
 
-print("PR Done ",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))        
-#***************************************************
-# e) open the GT instance file
-# f) for each colour in the instance, get borders
+        for an_object in gt_objects:
+            print('Colour: {}  >>  Fragments: {}'.format(an_object, len(gt_objects[an_object])))
+            # g) get the types assigned to each object in GT
+            # h) comparte to  PR labels to get FN
+            for fragment in gt_objects[an_object]:
+                f_centre = getcontourcentre(fragment)
+                pr_class = tuple(pr_lbl_img[f_centre])
+                gt_class = tuple(gt_lbl_img[f_centre])
+                print(an_object, "ground truth:",gt_class, "predicted:", pr_class)
+                ob_values[indx] = {"file":filename, "source":"GT","obj_colour":an_object, "ground truth":gt_class, "predicted":pr_class}
+                indx +=1
+            
+        print("file:",filename, "GT Done",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
 
-gt_objects = get_shapes_per_colour(gt_ins)
+        # a) open the predictions instance file
+        # b) for each colour in the instance, get borders.
+        pr_objects = get_shapes_per_colour(pr_ins)
 
-gt_values = {}
-indx = 1
-for an_object in gt_objects:
-    print('Colour: {}  >>  Fragments: {}'.format(an_object, len(gt_objects[an_object])))
-    # g) get the types assigned to each object in GT
-    # h) comparte to  PR labels to get FN
-    for fragment in gt_objects[an_object]:
-        f_centre = getcontourcentre(fragment)
-        pr_class = tuple(pr_lbl_img[f_centre])
-        gt_class = tuple(gt_lbl_img[f_centre])
-        print(an_object, "ground truth:",gt_class, "predicted:", pr_class)
-        gt_values[indx] = {"obj_colour":an_object, "ground truth":gt_class, "predicted":pr_class}
-        indx +=1
-    
-print("GT Done",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+        for an_object in pr_objects:
+            print('Colour: {}  >>  Fragments: {}'.format(an_object, len(pr_objects[an_object])))
+            # c) get the types assigned to each object fragment in predictions and GT
+            # d) compare to GT labels  to get TP and FP
+            for fragment in pr_objects[an_object]:
+                f_centre = getcontourcentre(fragment)
+                assign_pr_class = assignclass(fragment, pr_lbl_img)
+                gt_class = tuple(gt_lbl_img[f_centre])
+                print(an_object, "ground truth:",gt_class, "predicted:", assign_pr_class)
+                ob_values[indx] = {"file":filename, "source":"PR", "obj_colour":an_object, "ground truth":gt_class, "predicted":assign_pr_class}
+                indx +=1
+
+        print("file:",filename, "PR Done ",datetime.now().strftime("%Y/%m/%d %H:%M:%S"))        
+        
+    print("fragments detected:", indx)
+
+    write_csv_data(ob_values,out_file)
+
+    # i) categorise results to obtain TP, TN, FP and FN for each file
+    # Add to DB, use queries to summarise
+
+if __name__ == "__main__":
+   label_colors = {1:[(0.0,0.0,0.0),'background'],2:[(1.0,1.0,1.0),'barcode'],
+                3:[(1.0,0.0,0.0),'label'],4:[(1.0,1.0,0.0),'specimen'],
+                5:[(0.0,0.0,1.0),'typelabel']}
+   get_gt_values(sys.argv[1:], label_colors)
